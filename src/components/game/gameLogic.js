c7ace2b5480656277be056a253f2833d2b091878
yairIfrach <yair.ifrach2000@gmail.com>
2022-05-17 01:58:35 +0300

    add image and logic to the game

--- src/components/game/gameLogic.js
+++ src/components/game/gameLogic.js
@@ -1,10 +1,11 @@
-import React, { useState, forwardRef } from 'react';
+import React, { useState, forwardRef, useEffect } from 'react';
 import {
     Slide, createStyles, makeStyles, Typography, IconButton,
     DialogTitle, Dialog, Button, DialogContent, DialogActions, Grid
 } from '@material-ui/core';
 import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
 import { ImageGroup, Image } from 'react-fullscreen-image'
+import { allImage, evytarImgs, exemplImgs, yairImgs } from './images';
 
 
 const _ = require('lodash');
@@ -18,6 +19,9 @@ const useStyles = makeStyles(() => createStyles({
     img: {
         width: '100%',
         height: '100%',
+    } ,
+    frame :{
+        
     }
 }))
 
@@ -25,40 +29,78 @@ const GameLogic = (props) => {
     const { handleIsDialog } = props
     const classes = useStyles()
 
-    const yairImgs = [
-        {
-            id: 1,
-            src: '/images/IMG-20210910-WA0036.JPG',
-            location: '',
-        },
-        {
-            id: 2,
-            src: '/images/first.JPG',
-            location: '',
-        },
-        {
-            id: 3,
-            src: '/images/second.JPEG',
-            location: '',
-        },
-    ]
-
     const handleCloseGame = () => handleIsDialog(false)
 
+    const [isGame, setIsGame] = useState(false)
     const [titleMassege, setTitleMassege] = useState(false)
-    const [currentYairImg, setCurrentYairImgs] = useState(yairImgs[0])
-
+    const [disabledGoBack, setDisabledGoBack] = useState(true)
+    const [calcScore, setCalcScore] = useState({ errorAns: 0, correctAns: 0 })
+    const [currentExemplImg, setCurrentExemplImgs] = useState(exemplImgs[0])
 
     const handleContinue = () => {
-        _.isEqual(yairImgs[yairImgs.length - 2], currentYairImg) ? setTitleMassege(true) : setTitleMassege(false);
-        setCurrentYairImgs(yairImgs[yairImgs.findIndex(x => x.id === currentYairImg.id) + 1])
+        if (!isGame) {
+            setDisabledGoBack(false)
+            _.isEqual(exemplImgs[exemplImgs.length - 2], currentExemplImg) ? setTitleMassege(true) : setTitleMassege(false);
+            if (!_.isEqual(exemplImgs[exemplImgs.length - 1], currentExemplImg)) {
+                setCurrentExemplImgs(exemplImgs[exemplImgs.findIndex(x => x.id === currentExemplImg.id) + 1])
+            } else {
+                setIsGame(true)
+                setCurrentExemplImgs(allImage[0])
+            }
+        } else {
+            currentExemplImg.isYair ?
+                setCalcScore({
+                    ...calcScore,
+                    ['correctAns']: calcScore['correctAns'] + 1
+                }) :
+                setCalcScore({
+                    ...calcScore,
+                    ['errorAns']: calcScore['errorAns'] + 1
+                })
+
+            if (!_.isEqual(allImage[allImage.length - 1], currentExemplImg)) {
+
+                setCurrentExemplImgs(allImage[allImage.findIndex(x => x.id === currentExemplImg.id) + 1])
+
+            } else {
+                console.log("end")
+            }
+        }
+
     }
+
+    console.log(allImage)
 
     const handleGoBack = () => {
-        _.isEqual(yairImgs[yairImgs.length - 2], currentYairImg) ? setTitleMassege(true) : setTitleMassege(false);
-        setCurrentYairImgs(yairImgs[yairImgs.findIndex(x => x.id === currentYairImg.id) - 1])
+        if (!isGame) {
+
+            setTitleMassege(false);
+            if (_.isEqual(exemplImgs[1], currentExemplImg)) {
+                setDisabledGoBack(true)
+            }
+            setCurrentExemplImgs(exemplImgs[exemplImgs.findIndex(x => x.id === currentExemplImg.id) - 1])
+        } else {
+            currentExemplImg.isYair ?
+                setCalcScore({
+                    ...calcScore,
+                    ['errorAns']: calcScore['errorAns'] + 1
+                }) :
+                setCalcScore({
+                    ...calcScore,
+                    ['correctAns']: calcScore['correctAns'] + 1
+                })
+            if (!_.isEqual(allImage[allImage.length - 1], currentExemplImg)) {
+
+
+                setCurrentExemplImgs(allImage[allImage.findIndex(x => x.id === currentExemplImg.id) + 1])
+            } else {
+                console.log("end")
+            }
+        }
+
     }
 
+    console.log(calcScore)
 
     return (
         <div className={classes.skills}>
@@ -71,19 +113,19 @@ const GameLogic = (props) => {
             >
                 <DialogTitle>
                     <Grid container justify="space-between" alignItems="center">
-                        <Typography variant="div">{titleMassege ? `Start playing!` : `Take a good look he's Yair`}</Typography>
+                        <Typography variant="div">{isGame ? 'Who is this guy?' : (titleMassege ? `Start playing!` : `Take a good look he's Yair`)}</Typography>
                         <IconButton onClick={handleCloseGame} >
                             <CloseOutlinedIcon />
                         </IconButton>
                     </Grid>
                 </DialogTitle>
-                <DialogContent >
-                    <Image className={classes.img} src={currentYairImg.src} alt={currentYairImg.id}  />
+                <DialogContent className={classes.frame} >
+                    <img className={classes.img} src={currentExemplImg.src} alt={currentExemplImg.id} />
                 </DialogContent>
                 <DialogActions >
                     <Grid container justify="space-around" alignItems="center">
-                        <Button onClick={handleGoBack}>Go back</Button>
-                        <Button onClick={handleContinue}> {titleMassege ? 'start the game' : 'Continue'} </Button>
+                        <Button onClick={handleGoBack} disabled={disabledGoBack}>{isGame ? 'Evytar' : 'Go back'}</Button>
+                        <Button onClick={handleContinue}> {isGame ? 'Yair' : (titleMassege ? 'start the game' : 'Continue')} </Button>
                     </Grid>
 
                 </DialogActions>
---
+++ src/components/game/images.js
@@ -0,0 +1,107 @@
+export const exemplImgs = [
+    {
+        id: 1,
+        src: '/images/IMG-20210910-WA0036.JPG',
+        location: '',
+        isYair: true,
+    },
+    {
+        id: 2,
+        src: '/images/second.JPEG',
+        location: '',
+        isYair: true,
+    },
+]
+
+export const evytarImgs = [
+    {
+        id: 3,
+        src: '/images/20181105_173716.JPG',
+        location: '',
+        isYair: false,
+    },
+    {
+        id: 4,
+        src: '/images/20181108_164931.JPG',
+        location: '',
+        isYair: false,
+    },
+    {
+        id: 5,
+        src: '/images/IMG-20210331-WA0021.JPG',
+        location: '',
+        isYair: false,
+    },
+    {
+        id: 6,
+        src: '/images/IMG_20211210_120236.JPG',
+        location: '',
+        isYair: false,
+    },
+    {
+        id: 7,
+        src: '/images/5.JPG',
+        location: '',
+        isYair: false,
+    },
+    {
+        id: 8,
+        src: '/images/6.JPG',
+        location: '',
+        isYair: false,
+    },
+    {
+        id: 9,
+        src: '/images/7.JPG',
+        location: '',
+        isYair: false,
+    },
+    {
+        id: 10,
+        src: '/images/8.JPG',
+        location: '',
+        isYair: false,
+    },
+]
+
+export const yairImgs = [
+    {
+        id: 11,
+        src: '/images/_DSC7154.JPG',
+        location: '',
+        isYair: true,
+    },
+    {
+        id: 13,
+        src: '/images/IMG_20220203_205624.JPG',
+        location: '',
+        isYair: true,
+    },
+    {
+        id: 14,
+        src: '/images/1.JPEG ',
+        location: '',
+        isYair: true,
+    },
+    {
+        id: 15,
+        src: '/images/2.JPG',
+        location: '',
+        isYair: true,
+    },
+    {
+        id: 16,
+        src: '/images/3.JPG',
+        location: '',
+        isYair: true,
+    },
+    {
+        id: 17,
+        src: '/images/4.JPEG ',
+        location: '',
+        isYair: true,
+    },
+]
+
+
+export const allImage = (evytarImgs.concat(yairImgs)).sort(() => Math.random() - 0.5)
